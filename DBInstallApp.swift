179ded6c8403231c40d38f840ebff79c3975b175
Yeongmin_Chae <codudals98@naver.com>
2023-06-11 18:41:17 +0900

    Setup Update

---
+++ DBInstallApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBInstallApp.swift
+//  DBInstall
+//
+//  Created by 채영민 on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct DBInstallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

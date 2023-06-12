ccff814b4eb4177ba2bed480b2b01133ddac7dc5
Yeongmin_Chae <codudals98@naver.com>
2023-06-12 19:51:40 +0900

    create - display the memos on my home screen.

--- ContentView.swift
+++ ContentView.swift
@@ -8,19 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var memoVM: MemoViewModel
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        var memos = memoVM.memos.sorted {$0.postedDate > $1.postedDate}
+        
+        NavigationView {
+            VStack {
+                List(memos, id: \.self) {memo in
+                    NavigationLink(destination: {
+                        MemoDetailView(memo)
+                    }, label: {
+                        Text(memo.title)
+                    })
+                }
+                .refreshable {
+                    memoVM.refreshMemo()
+                }
+                .background(.white)
+                .onAppear(perform: {
+                    UITableView.appearance().backgroundColor = UIColor.clear
+                    UITableViewCell.appearance().backgroundColor = UIColor.clear
+                })
+                .navigationTitle("Memo")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(
+                            destination: {
+                                CreateMemoView(Memo())
+                            },
+                            label:{
+                                Image(systemName:"plus")}
+                        )
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(memoVM: MemoViewModel())
     }
 }
